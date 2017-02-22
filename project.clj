99fef991ff6e8df5ad47e871c432ae248fb49e69
Michael Gough <innovative.engineer@gmail.com>
2017-02-21 23:19:04 -0800

    A few changes to bring project.clj closer to the one that works on heroku

--- project.clj
+++ project.clj
@@ -9,20 +9,24 @@
   :dependencies [
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
+                 [org.omcljs/om "1.0.0-alpha46"]
+
+                 [navis/untangled-client "0.6.0" :exclusions [cljsjs/react org.omcljs/om]]
+                 [navis/untangled-server "0.5.1"]
+                 [navis/untangled-spec "0.3.6"]
+                 [navis/untangled-websockets "0.2.0"]
+
                  [com.datomic/datomic-free "0.9.5359" :exclusions [joda-time com.google.guava/guava]]
                  [com.taoensso/timbre "4.3.1"]
                  [commons-codec "1.10"]
                  
-                 [org.omcljs/om "1.0.0-alpha41"]
+                 
                  [binaryage/devtools "0.5.2"]
                  [figwheel-sidecar "0.5.3" :exclusions [ring/ring-core joda-time org.clojure/tools.reader]]
                  [com.cemerick/piggieback "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [juxt/dirwatch "0.2.3"]
-                 [navis/untangled-client "0.6.0" :exclusions [cljsjs/react org.omcljs/om]]
-                 [navis/untangled-server "0.5.1"]
-                 [navis/untangled-spec "0.3.6"]
-                 [navis/untangled-websockets "0.2.0"]
+
                  [com.google.protobuf/protobuf-java "2.5.0" :exclusions [com.google.guava/guava]]
 
                  ]
