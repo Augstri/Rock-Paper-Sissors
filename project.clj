41012bc8ea4fd491594f3253cc84114e7e409ee5
Michael Gough <innovative.engineer@gmail.com>
2017-02-28 19:03:18 -0800

    Go back to dependencies from upstream/master

--- project.clj
+++ project.clj
@@ -11,36 +11,22 @@
 
 
 
-
-
-
-  :dependencies [
-
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [org.omcljs/om "1.0.0-alpha46"]
-                 [navis/untangled-client "0.6.1"]
-                 [navis/untangled-server "0.6.2" :exclusions [io.aviso/pretty org.clojure/clojurescript]]
-                 [navis/untangled-datomic "0.4.11" :exclusions [org.clojure/tools.cli]]
-                 [com.datomic/datomic-free "0.9.5359" :exclusions [com.google.guava/guava]]
-                 [secretary "1.2.3" :exclusions [com.cemerick/clojurescript.test]]
-                 [joda-time "2.9.3"]
-                 [clj-time "0.11.0"]
-                 [lein-doo "0.1.7" :scope "test" :exclusions [org.clojure/tools.reader]]
-                 [org.clojure/tools.namespace "0.2.11"]
+  :dependencies [[com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]
+                 [com.taoensso/timbre "4.3.1"]
                  [commons-codec "1.10"]
-                 [com.taoensso/timbre "4.8.0"]
-                 [com.stuartsierra/component "0.3.2"]
-                 [navis/untangled-spec "0.3.9" :scope "test"]
-                 [navis/untangled-websockets "0.3.1"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [org.omcljs/om "1.0.0-alpha41"]
                  [binaryage/devtools "0.5.2"]
-                 [figwheel-sidecar "0.5.3" :exclusions [ring/ring-core joda-time org.clojure/tools.reader]]
+                 [figwheel-sidecar "0.5.9" :exclusions [ring/ring-core joda-time org.clojure/tools.reader]]
                  [com.cemerick/piggieback "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [juxt/dirwatch "0.2.3"]
-                 [com.google.protobuf/protobuf-java "2.5.0" :exclusions [com.google.guava/guava]]
+                 [navis/untangled-client "0.6.0" :exclusions [cljsjs/react org.omcljs/om]]
+                 [navis/untangled-server "0.5.1"]
+                 [navis/untangled-spec "0.3.6"]
+                 [navis/untangled-websockets "0.2.0"]]
 
-                 ]
 
   :plugins [[lein-cljsbuild "1.1.3"]]
 
