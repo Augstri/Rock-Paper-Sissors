7b748d972a6fbea8c6f4218748e69c3c2d34aca2
Michael Gough <innovative.engineer@gmail.com>
2017-03-03 16:58:11 -0800

    remove figwheel

--- dev/server/user.clj
+++ dev/server/user.clj
@@ -4,29 +4,8 @@
     [clojure.stacktrace :refer (print-stack-trace)]
     [clojure.tools.namespace.repl :refer [disable-reload! refresh clear set-refresh-dirs]]
     [com.stuartsierra.component :as component]
-    [figwheel-sidecar.repl-api :as ra]
     [app.system :as sys]
     [taoensso.timbre :as timbre]))
-
-;;FIGWHEEL
-
-(def figwheel-config
-  {:figwheel-options {:css-dirs ["resources/public/css"]}
-   :build-ids        ["dev"]
-   :all-builds       (figwheel-sidecar.repl/get-project-cljs-builds)})
-
-(defn start-figwheel
-  "Start Figwheel on the given builds, or defaults to build-ids in `figwheel-config`."
-  ([]
-   (let [props (System/getProperties)
-         all-builds (->> figwheel-config :all-builds (mapv :id))]
-     (start-figwheel (keys (select-keys props all-builds)))))
-  ([build-ids]
-   (let [default-build-ids (:build-ids figwheel-config)
-         build-ids (if (empty? build-ids) default-build-ids build-ids)]
-     (println "STARTING FIGWHEEL ON BUILDS: " build-ids)
-     (ra/start-figwheel! (assoc figwheel-config :build-ids build-ids))
-     (ra/cljs-repl))))
 
 (set-refresh-dirs "dev/server" "src/server" "src/shared")
 
